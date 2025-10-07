65a40dec87b13bd3cfdc0e15abdbe74ec7a2abe3
aslam145 <aslampashamd145@gmail.com>
2025-10-07 20:53:57 +0530

    Update StringMethods.java

--- StringMethods.java
+++ StringMethods.java
@@ -11,7 +11,7 @@ public class StringMethods {
         for(int i=0; i<fullname.length(); i++){
         System.out.print(fullname.charAt(i));
         }System.out.println();
-        // compare
+        // compare 2 strings
         // here we get 3 cases
         // 1->n1>n2=+ value
         // 2->n1<n2=-value
@@ -21,5 +21,21 @@ public class StringMethods {
         }else{
             System.out.println("strings are not equal");
         }
+        if(name1.equals(name2)){
+            System.out.println("strings are equal");
+        }
+        // substring
+        String sentence="github";
+        String sub;
+        sub=sentence.substring(0, 5);
+        System.out.println(sub);
+        // parseInt Method (String to integer)
+        String str="6377";
+        int num=Integer.parseInt(str);
+        System.out.println(num);
+        // toStringmethod (integer to String)
+        int numbers=6377;
+        String string=Integer.toString(num);
+        System.out.println(str.length());
     }
 }
